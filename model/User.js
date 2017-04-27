abf97453e4907f3dbe0a48a4ec7cfb528a703dfd
Felipe Muner <felipe.muner@gmail.com>
2017-04-26 21:34:43 -0300

    First commit, configuration of app.js

---
+++ model/User.js
@@ -0,0 +1,9 @@
+function User(UserID, Matricula, Pass, GrupoID, Ativo){
+  this.UserID = UserID
+  this.Matricula = Matricula
+  this.Pass = Pass
+  this.GrupoID = GrupoID
+  this.Ativo = Ativo
+}
+
+module.exports = User
---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+var User = require(process.env.PWD + '/model/User');
+
+//console.log(JSON.stringify(u,null,2));
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var u = new User(1, 2595, null, 1,1);
+  console.log(u);
+  res.render('index', {
+    User: JSON.stringify(u,null,2)
+  });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login' });
+});
+
+router.get('/admin', function(req, res, next) {
+  res.render('admin', { title: 'admin' });
+});
+
+module.exports = router;
