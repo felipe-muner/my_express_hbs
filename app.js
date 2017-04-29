ea67ae5c0fbd6de9a34b5562ed70f356b199d2c5
felipe muner <felipe.muner@gmail.com>
2017-04-29 18:44:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var exphbs = require('express-handlebars');
 var expressSession = require('express-session');
 
 var index = require('./routes/index');
-//var users = require('./routes/users'); // voltando a comentar
+var users = require('./routes/users'); // voltando a comentar
 
 var app = express();
 
