465f4bcad71c884475e01e89301365ea82d0cc51
Felipe Muner <felipe.muner@gmail.com>
2017-05-16 22:25:44 -0300

    Adding module nodemailer

--- test-email.js
+++ test-email.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const nodemailer = require('nodemailer');
 
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
