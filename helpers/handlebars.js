f5ff5f1037cb0ea43161f0ce978db307a4e163b7
Felipe Muner <felipe.muner@gmail.com>
2017-06-19 20:27:55 -0300

    adjusting milisecond in macair

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -2,6 +2,11 @@ const moment = require('moment')
 
 function hbsHelpers(hbs) {
   console.log('FOI ENTREI NA PASTA NOVA');
+  console.log(moment().format());
+  console.log(new Date());
+  console.log(moment().format('YYYY-MM-DD HH:mm:ss:SSS'));
+  console.log(moment().toISOString());
+  console.log(moment());
   return hbs.create({
     defaultLayout:'layout',
     helpers: {
@@ -12,9 +17,10 @@ function hbsHelpers(hbs) {
       },
       dizNome: function() {
         console.log('reading it');
-        console.log(this);
-
-        return moment().format('YYYY-MM-DD hh:mm:ss:SSSS')
+        //console.log(this);
+        var dt = moment().format('YYYY-MM-DD hh:mm:ss:SSS')
+        console.log(dt);
+        return dt
       }
     }
   });
